29a1e59120482831527bcd9d9f933e99658c7004
RustamIsmayilov96 <106178348+RustamIsmayilov96@users.noreply.github.com>
2022-06-21 18:09:20 +0300

    Update main.js

--- task-1/main.js
+++ task-1/main.js
@@ -3,7 +3,14 @@ prompt.start();
 
 
 prompt.get('input', function (err, result) {
-  
-    // Write code here
-  
+    /* 
+    Istifadəçinin daxil etdiyi dəyişəni qəbul edirik. 
+    Giriş veriləni String type olur. Riyazi əməliyyatları tətbiq etmək üçün parseInt() metodundan istifadə edərək Number type-a çevrilməlidir.
+    */
+    var n = parseInt(result.input); // 9
+    let sum = 0
+    while(n>0){
+    sum = sum + n%10
+    n=  parseInt(n/10)
+    }console.log(sum);
 });
