0cd27d4425ccd79ae362859a7dd28a60213be006
RustamIsmayilov96 <106178348+RustamIsmayilov96@users.noreply.github.com>
2022-06-21 18:43:09 +0300

    Update main.js

--- task-4/main.js
+++ task-4/main.js
@@ -3,7 +3,17 @@ prompt.start();
 
 
 prompt.get('input', function (err, result) {
-  
-    // Write code here
-  
+    /* 
+    Istifadəçinin daxil etdiyi dəyişəni qəbul edirik. 
+    Giriş veriləni String type olur. Riyazi əməliyyatları tətbiq etmək üçün parseInt() metodundan istifadə edərək Number type-a çevrilməlidir.
+    */
+    let n = 12
+    let k =12
+       while(k>0){
+        k=k-1
+            let res = parseInt(n/k)
+    if(res%2!=0){
+        console.log(res);
+    }
+        }
 });
