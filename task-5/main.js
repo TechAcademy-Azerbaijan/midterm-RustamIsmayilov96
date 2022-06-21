e16255b7e1573c63edeefb1189fac56362ba20e4
RustamIsmayilov96 <106178348+RustamIsmayilov96@users.noreply.github.com>
2022-06-21 19:02:38 +0300

    Update main.js

--- task-5/main.js
+++ task-5/main.js
@@ -12,10 +12,11 @@ prompt.get('input', function (err, result) {
     var numbers = input.split(','); // ['12', '5']
     var a = parseInt(numbers[0]); // Ilk daxil olunmuş ədədi alırıq. Nümunə - 12 
     var b = parseInt(numbers[1]); // İkinci daxil olunmuş ədədi alırıq. Nümunə - 5
-let sum = 0
-let res = 0
-while(a<=b){
-    if(res%2!=0){
-sum = sum + res
-    }  res=a++
-}console.log(sum);
+    let sum = 0
+    let res = 0
+    while(a<=b){
+        if(res%2!=0){
+    sum = sum + res
+        }  res=a++
+    }console.log(sum);
+});
